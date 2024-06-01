e3f97f643411c039a9078792ce1ba2619e774f3a
VArtz <farrelnikoson@gmail.com>
2024-06-01 12:21:40 +0700

    cd builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -20,9 +20,7 @@ func main() {
         cmd := strings.Split(input, " ")
         switch cmd[0] {
         case "cd":
-            if err := os.Chdir(cmd[0]); err != nil {
-                fmt.Fprintf(os.Stdout, cmd[0] + ": No such file or  directory\n")
-            }
+            continue
         case "type":
             switch cmd[1] {
             case "exit", "echo", "type":

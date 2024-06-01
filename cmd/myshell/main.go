3d07316f909e61ffac6a9b720bf5d11c027ebc21
VArtz <farrelnikoson@gmail.com>
2024-06-01 12:59:05 +0700

    refactor code

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -20,34 +20,21 @@ func main() {
         cmd := strings.Split(input, " ")
         switch cmd[0] {
         case "cd":
-            homedir, err := handleCd(cmd[1])
-            if err != nil {
-                cmd[1] = homedir
-            } 
-        case "type":
-            switch cmd[1] {
-            case "exit", "echo", "type":
-                fmt.Fprint(os.Stdout, cmd[1] + " is a shell builtin\n")
-            default:
-                paths := strings.Split(os.Getenv("PATH"), ":")
-                for _, path := range paths {
-                    fp := filepath.Join(path, cmd[1])
-                    if _, err := os.Stat(fp); err == nil {
-                        fmt.Fprint(os.Stdout, fp + "\n")
-                        continue Exit
-                    }
-                }
-                fmt.Fprintf(os.Stdout, cmd[1] + " not found\n")
+            if strings.TrimSpace(cmd[1]) == "~" {
+                return os.Getenv("HOME"), nil
             }
+            if err := os.Chdir(cmd[1]); err != nil {
+                fmt.Fprintf(os.Stdout, path + ": No such file or directory\n")
+            }
+            return "", fmt.Errorf("")
+        case "type":
+            handleType(cmd[1])
         case "echo":
             fmt.Fprint(os.Stdout, strings.Join(cmd[1:], " ") + "\n")
         case "exit":
             os.Exit(0)
         default:
-            exe := exec.Command(cmd[0], cmd[1:]...)
-            exe.Stderr = os.Stderr
-            exe.Stdout = os.Stdout
-            err := exe.Run()
+            err := handleExec(cmd[0], cmd[:1])
             if err != nil {
                 fmt.Fprint(os.Stdout, input[:len(input)] + ": command not found\n")
             }
@@ -55,12 +42,26 @@ func main() {
     }
 }
 
-func handleCd(path string) (homedir string, err error) {
-            if strings.TrimSpace(path) == "~" {
-                return os.Getenv("HOME"), nil
+func handleExec(cmd string, args string[]) {
+            exe := exec.Command(cmd, args...)
+            exe.Stderr = os.Stderr
+            exe.Stdout = os.Stdout
+            return exe.Run()
+}
+
+func handleType(arg string) {
+            switch arg {
+            case "exit", "echo", "type":
+                fmt.Fprint(os.Stdout, arg + " is a shell builtin\n")
+            default:
+                paths := strings.Split(os.Getenv("PATH"), ":")
+                for _, path := range paths {
+                    fp := filepath.Join(path, arg)
+                    if _, err := os.Stat(fp); err == nil {
+                        fmt.Fprint(os.Stdout, fp + "\n")
+                        continue Exit
+                    }
+                }
+                fmt.Fprintf(os.Stdout, arg + " not found\n")
             }
-            if err := os.Chdir(path); err != nil {
-                fmt.Fprintf(os.Stdout, path + ": No such file or directory\n")
-            }
-            return "", fmt.Errorf("")
 }

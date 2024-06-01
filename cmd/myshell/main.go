0b72fe6aa536790d30833a3bc17d7d5f3ffd5f2e
VArtz <farrelnikoson@gmail.com>
2024-06-01 11:00:59 +0700

    pass 1st stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,11 +7,7 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
+	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')

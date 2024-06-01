e5a33eb2701038e14166175807fafa75fd873f48
VArtz <farrelnikoson@gmail.com>
2024-06-01 11:34:53 +0700

    The echo builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -14,7 +14,10 @@ func main() {
         // Wait for user input
         input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
         input = strings.TrimSpace(input)
+        cmd := strings.Split(input, " ")
         switch input {
+        case "echo":
+            fmt.Fprint(os.Stdout, strings.Join(command[1:], " ") + "\n")
         case "exit 0":
             os.Exit(0)
         default:

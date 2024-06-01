7762075a54ffcc3a458938899ab58b8c25568e79
VArtz <farrelnikoson@gmail.com>
2024-06-01 12:17:14 +0700

    cd builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -22,7 +22,6 @@ func main() {
         case "cd":
             if err := os.Chdir(cmd[0]); err != nil {
                 fmt.Fprintf(os.Stdout, cmd[0] + ": No such file or  directory\n")
-            } else {
                 continue
             }
         case "type":

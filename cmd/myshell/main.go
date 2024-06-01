b673319241051ad929a5fef0e79eb6a4061a4984
VArtz <farrelnikoson@gmail.com>
2024-06-01 11:26:38 +0700

    REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-    while(true) {
-	fmt.Fprint(os.Stdout, "$ ")
+    for true {
+        fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-    input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-    fmt.Fprint(os.Stdout, input[:len(input) - 1] + ": command not found\n")
-}
+        // Wait for user input
+        input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+        fmt.Fprint(os.Stdout, input[:len(input) - 1] + ": command not found\n")
+    }
 }
